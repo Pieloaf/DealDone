e639afd9e3e099298e605d878deacf8bfa4a6e76
Pieloaf <49878076+Pieloaf@users.noreply.github.com>
2020-02-27 21:25:19 +0000

    main cpp file

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "Vehicle.h"
+#include "Manufacturer.h"
+
+using namespace std;
+
+int main()
+{
+    Manufacturer m1("Soyota");
+    m1.addColour("blue");
+    m1.addColour("red");
+    m1.addColour("green");
+    m1.addColour("yellow");
+    Vehicle v1(m1);
+    v1.setModel_Name("Auris");
+
+    v1.displayVehicleDetails();
+
+
+}
