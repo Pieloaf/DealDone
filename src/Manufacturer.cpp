67c451b4a5388ccb0df20d2bf8e7b9e49d741941
Dawg4321 <ryan.wiebe@outlook.com>
2020-04-21 18:20:26 +0100

    Commented Manufacturer.h and .cpp

--- src/Manufacturer.cpp
+++ src/Manufacturer.cpp
@@ -1,10 +1,10 @@
 #include "Manufacturer.h"
 
-Manufacturer::Manufacturer()
+Manufacturer::Manufacturer() // default constructor
 {
     //ctor
 }
-Manufacturer::Manufacturer(string n){
+Manufacturer::Manufacturer(string n){ // user defined contrustor to add a name
     name = n;
 }
 Manufacturer::~Manufacturer()
@@ -12,7 +12,7 @@ Manufacturer::~Manufacturer()
     //dtor
 }
 
-void Manufacturer::addColour(string colour_name){
+void Manufacturer::addColour(string colour_name){ // adds an offered colour to the colour vector
     colour_list.push_back(colour_name);
 }
 
@@ -31,45 +31,45 @@ void Manufacturer::listColours(){
     }
 }
 
-void Manufacturer::manuDetails(){
+void Manufacturer::manuDetails(){ // ouputs manufacturer info into the console
     cout << "Manufacturer: " << name << endl;
     cout << "Products: " << product_type << endl;
     cout << "Colours:" << endl;
-    Manufacturer::listColours();
+    Manufacturer::listColours(); // lists all the offered colours
 }
 
 
 bool operator== (Manufacturer m1, Manufacturer m2){
 
-    if(m1.getColour_List().size() == m2.getColour_List().size()){
-        if (m1.getColour_List() == m2.getColour_List()){
-            return(m1.getName() == m2.getName() && m1.getProductType() == m2.getProductType());
+    if(m1.getColour_List().size() == m2.getColour_List().size()){ // initially checks if the two vectors are the same size as if they aren't the manufacturers arent the same.
+        if (m1.getColour_List() == m2.getColour_List()){ // then checks if the two vectors are the same
+            return(m1.getName() == m2.getName() && m1.getProductType() == m2.getProductType()); // returns the "and" operation of the other data members
         }
         else{
-            vector <string> m1_col = m1.getColour_List();
+            vector <string> m1_col = m1.getColour_List(); // clones the vectors as to allow vector comparison
             vector <string> m2_col = m2.getColour_List();
             string tested_col;
 
-            for(int i = 0; i < m1_col.size(); i++){
-                tested_col = m1_col[i];
+            for(int i = 0; i < m1_col.size(); i++){ // runs through one vector
+                tested_col = m1_col[i]; // stores the colour for comparison
 
-                for(int j = 0; j < m2_col.size(); j++){
-                    if(tested_col == m2_col[j]){
-                        m2_col.erase(m2_col.begin() + j);
+                for(int j = 0; j < m2_col.size(); j++){ // runs through the other vector
+                    if(tested_col == m2_col[j]){ // if the tested colour is found in thhe other vector
+                        m2_col.erase(m2_col.begin() + j); // erases it from the inner vector and breaks
                         break;
                     }
                 }
             }
-            if(m2_col.size() == 0)
+            if(m2_col.size() == 0) // if all colours were equal in the inner vector (e.g. all of them matched)
             {
-                return(m1.getName() == m2.getName() && m1.getProductType() == m2.getProductType());
+                return(m1.getName() == m2.getName() && m1.getProductType() == m2.getProductType()); // returns the "and" operation of the other data members
             }
-            else return false;
+            else return false; // if a colour was not the same
         }
     }
 
-    else return false;
+    else return false; // return false if the two vectors are of different lengths
 }
-bool operator!= (Manufacturer m1, Manufacturer m2){
+bool operator!= (Manufacturer m1, Manufacturer m2){ // returns the opposite of the == operator
     return !(m1 == m2);
 }
