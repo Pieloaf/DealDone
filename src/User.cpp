9a5b0a99f25b5c8314afe26b8b28d0d422a672e5
Pieloaf <piercelowe1@gmail.com>
2020-04-21 21:17:23 +0100

    commented code

--- src/User.cpp
+++ src/User.cpp
@@ -1,6 +1,10 @@
 #include "User.h"
 
-User::User(std::string usrname):name(usrname)	
+User::User():name("guest") //initialiser list
+{
+}
+
+User::User(std::string usrname):name(usrname) //initiliser list
 {
 }
 
@@ -9,15 +13,37 @@ User::~User()
 
 }
 
+//
 void User::myVehicles(){
-	for (int i = 0; i < int(ownedVehicles.size()); i++)
+
+	//iterates through owned vehicles vector and prints out the index+1 and model_name of all objects in the vector
+	for (int i = 0; i < ownedVehicles.size(); i++)
 	{
-		std::cout << "\t" << i+1 << ". " << ownedVehicles[i]->getModel_Name() << std::endl;
+		cout << "\t" << i+1 << ". " << ownedVehicles[i]->getModel_Name() << endl;
 	}
 }
 
 void User::listDetails()
 {
-	std::cout << "User Name: \t" << name << std::endl;
-	std::cout << "Owned Vehicles:\t" << ownedVehicles.size() << endl;
+	//prints the User objects name data member and the number of Vehicle objects in the ownedVehicles vector 
+	cout << "User Name: \t" << name << endl;
+	cout << "Owned Vehicles: " << ownedVehicles.size() << endl;
+}
+void User::removeVehicle(Vehicle* v)
+{
+	// itterates to the given vehicle object from the ownedVehicles vector and removes it from the vector
+	for(int x=0; x<ownedVehicles.size()-1; x++)
+    {
+		int i = x--;
+        if (v==ownedVehicles[x])
+			ownedVehicles.erase(ownedVehicles.begin()+i);
+		break;
+    }
+}
+
+ostream& operator<< (ostream& ostr, const User& u)
+{
+	ostr << "User Name: \t" << u.name << endl;
+	ostr << "Owned Vehicles: " << u.ownedVehicles.size() << endl;
+	return ostr;
 }
